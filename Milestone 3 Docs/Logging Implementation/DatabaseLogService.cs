1b5dd3111a53c89f9c32bd3a92e4c417055a71dd
Brayan Fuentes <39744430+BrayanFuentes@users.noreply.github.com>
2021-11-28 17:18:36 -0800

    Logging Implementation
    First draft of logging implementation

---
+++ Milestone 3 Docs/Logging Implementation/DatabaseLogService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace LongHorn.ArrowNav.Logging
+{
+    public class DatabaseLogService : ILogService
+    {
+        public bool Log(string description)
+        {
+            try
+            {
+                var connection = @"Server=localhost\SQLEXPRESS01;Database=Logging;Trusted_Connection=True;";
+                using (var conn = new SqlConnection(connection))
+                {
+                    var sql = "Insert Into Logging Values ('"+ DateTime.UtcNow + " " + description + "');";
+                    using (var command = new SqlCommand(sql, conn))
+                    {
+                        command.ExecuteReader();
+                    }
+                }
+
+                    return true;
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+    }
+
+}

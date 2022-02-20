4070e0678c828ecaf7c9a28e4d0ccc710602181a
Brayan Fuentes <39744430+BrayanFuentes@users.noreply.github.com>
2022-02-20 00:31:03 -0800

    front-end template

---
+++ Code Base/LongHorn.ArrowNav/LongHorn.ArrowNav/Front-End/ClientApp/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+
+
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+      return (
+          <div>HI</div>
+    );
+  }
+}

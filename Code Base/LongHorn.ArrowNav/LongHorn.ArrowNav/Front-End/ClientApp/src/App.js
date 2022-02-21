e3527d059224f720b3964d12d56c8983dad41c05
Brayan Fuentes <39744430+BrayanFuentes@users.noreply.github.com>
2022-02-20 20:57:59 -0800

    home and map component

--- Code Base/LongHorn.ArrowNav/LongHorn.ArrowNav/Front-End/ClientApp/src/App.js
+++ Code Base/LongHorn.ArrowNav/LongHorn.ArrowNav/Front-End/ClientApp/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
-
-
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/pages/Home";
+import { NavBar } from "./components/NavBar";
+import { Login } from "./components/pages/Login";
+import { WellnessHub } from "./components/pages/WellnessHub";;
 
 export default class App extends Component {
-  static displayName = App.name;
+    static displayName = App.name;
 
-  render () {
-      return (
-          <div>HI</div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Router>
+                    <NavBar />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/account" element={<Login />} />
+                        <Route path="/wellnesshub" element={<WellnessHub />} />
+                    </Routes>
+
+                </Router>
+            </div>
+        );
+    }
 }
---
+++ Code Base/LongHorn.ArrowNav/LongHorn.ArrowNav/Front-End/ClientApp/src/components/Map.js
@@ -0,0 +1,50 @@
+﻿import React, { useRef, useEffect, useState } from 'react';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import mapboxgl from '!mapbox-gl';
+import './Map.css';
+
+mapboxgl.accessToken = "pk.eyJ1IjoiYnJheWFuLWZ1ZW50ZXMyMSIsImEiOiJja3hxdW5ycWo0ZjRmMzBvNHM5ODdxZ2poIn0.MoTF9LUSyOlwGx7L-pCCjw";
+
+export const Map = () => {
+    const mapContainerRef = useRef(null);
+    const [lng, setLng] = useState(-118.112437);
+    const [lat, setLat] = useState(33.782105);
+    const [zoom, setZoom] = useState(14.75);
+
+    // Initialize map when component mounts
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            style: 'mapbox://styles/brayan-fuentes21/cky54exmf1uvl14qcvixitiqo',
+            center: [lng, lat],
+            zoom: zoom
+        });
+
+        
+        const geolocateControl = new mapboxgl.GeolocateControl({
+            positionOptions: { enableHighAccuracy: true },
+            showUserHeading: true
+        })
+
+        map.addControl(geolocateControl, 'bottom-right');
+
+
+        map.on('move', () => {
+            setLng(map.getCenter().lng.toFixed(4));
+            setLat(map.getCenter().lat.toFixed(4));
+            setZoom(map.getZoom().toFixed(2));
+        });
+
+        // Clean up on unmount
+        return () => map.remove();
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    return (
+        <div>
+            <div className='map-container' ref={mapContainerRef} />
+        </div>
+    );
+}
+
+
+export default Map;

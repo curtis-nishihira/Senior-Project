7fd47470945f364d4ad63fa592e8923e8dc68528
Curtis Nishihira <cknish27@gmail.com>
2022-03-06 22:49:17 -0800

    Popup finished

--- Code Base/LongHorn.ArrowNav/LongHorn.ArrowNav/Front-End/ClientApp/src/components/pages/WellnessHub.js
+++ Code Base/LongHorn.ArrowNav/LongHorn.ArrowNav/Front-End/ClientApp/src/components/pages/WellnessHub.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Popup from './Popup';
+import TrafficSurvey from './TrafficSurvey'
 
+export const WellnessHub = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
 
+    const togglePopup = () => {
+        setIsOpen(!isOpen);
+    }
 
-export function WellnessHub() {
-    return (
-        <div> wellness hub place holder</div>
-    );
+    return <div>
+        <input
+            type="button"
+            value="Click to Open Popup"
+            onClick={togglePopup}
+        />
+        {isOpen && <Popup
+            content={<>
+                <TrafficSurvey/>
+            </>}
+            handleClose={togglePopup}
+        />}
+    </div>
 }

533edbf63ccfc4e0f513b3957f5154fdbe323825
Curtis Nishihira <cknish27@gmail.com>
2022-04-07 09:29:41 -0700

    started building repo

---
+++ Code Base/LongHorn.ArrowNav/LongHorn.ArrowNav/LongHorn.ArrowNav.DAL/Implementation/BuildingRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LongHorn.ArrowNav.Models;
+
+namespace LongHorn.ArrowNav.DAL.Implementation
+{
+    public class BuildingRepository : IRepository<BuildingModel>
+    {
+        public string Create(BuildingModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(BuildingModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> Read(BuildingModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(BuildingModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Code Base/LongHorn.ArrowNav/LongHorn.ArrowNav/LongHorn.ArrowNav.Models/BuildingModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongHorn.ArrowNav.Models
+{
+    public class BuildingModel
+    {
+        public string acronym { get; set; } = "";
+        public string buildingName { get; set; } = "";
+        public float longitude { get; set; } = float.MinValue;
+        public float latitude { get; set; } = float.MaxValue;
+
+    }
+}

6ca5e788d8d2ec03d505ff1dac5247dad7132ef4
Curtis Nishihira <cknish27@gmail.com>
2022-04-07 09:36:14 -0700

    added template controller

---
+++ Code Base/LongHorn.ArrowNav/LongHorn.ArrowNav/Front-End/Controllers/BuildingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LongHorn.ArrowNav.Models;
+using LongHorn.ArrowNav.Managers;
+using System.Text.Json;
+
+namespace Front_End.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BuildingController : ControllerBase
+    {
+        // get for all buildings
+
+        // post returns long and lat 
+
+    }
+}

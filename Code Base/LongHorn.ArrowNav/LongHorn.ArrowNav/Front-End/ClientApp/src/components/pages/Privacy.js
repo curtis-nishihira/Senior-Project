77b1e499bac4729eb1ca8c4515e6ecb2c08359f2
spencermgravel <spencermgravel@gmail.com>
2022-04-10 16:32:27 -0700

    Updated Privacy message

--- Code Base/LongHorn.ArrowNav/LongHorn.ArrowNav/Front-End/ClientApp/src/components/pages/Privacy.js
+++ Code Base/LongHorn.ArrowNav/LongHorn.ArrowNav/Front-End/ClientApp/src/components/pages/Privacy.js
@@ -11,7 +11,26 @@ export const Privacy = (props) => {
 	return (
 		<form onSubmit={surveyHandler}>
 			<h3>Privacy Agreement</h3>
-			<p>Insert Policy</p>
+			<p>
+				By creating an ArrowNav account, personal information relating to the account, such as
+				username and password, will be kept confidential.
+
+				The only data that will be analysed and collected when using ArrowNav will be
+				from submitting non-mandatory surveys. This data will be used to update how crowded
+				locations on campus are and update travel times when generating routes on the application.
+
+				Should a user wish to delete their account, all information pertaining to that account,
+				such as username and password, will be deleted from ArrowNav databases.
+
+				User account information is never shared or distributed to third parties.
+
+				Cookies are used for this application. When agreeing to the privacy policy, you as a user agree
+				to allow cookies.
+
+				By agreeing to this privacy policy, you are agreeing to all that has been mentioned above
+				and agree to abide by said policy.
+				
+			</p>
 			<button type="submit">Accept</button>
 			<button type="submit">Don't Allow</button>
 		</form>

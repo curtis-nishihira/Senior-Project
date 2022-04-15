457df930526b71dcfbd3b41defc1cd4a32b6146e
spencermgravel <spencermgravel@gmail.com>
2022-04-15 11:48:35 -0700

    updating rewards branch

--- Code Base/LongHorn.ArrowNav/LongHorn.ArrowNav/Front-End/ClientApp/src/setupProxy.js
+++ Code Base/LongHorn.ArrowNav/LongHorn.ArrowNav/Front-End/ClientApp/src/setupProxy.js
@@ -7,7 +7,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 const context =  [
     "/login",
     "/trafficsurvey",
-    "/rewards"
+    "/rewards",
 ];
 
 module.exports = function(app) {
